3690778c123c7af6d2d3145bff679567b6cbf8b3
AllenOtto <kephamoturi@gmail.com>
2021-12-29 15:55:00 +0300

    Set count to increment on upvote and decrement on downvote

--- Content.js
+++ Content.js
@@ -6,7 +6,6 @@ let blackThroughTimeAndSpace = [
     "https://www.barnorama.com/wp-content/uploads/2019/05/beautiful_women_03.jpg",
     "https://www.stylevore.com/wp-content/uploads/2019/04/1556356915_655_notitle.jpg",
     "https://netstorage-tuko.akamaized.net/images/0fgjhs36nflcdmdo9.jpg?imwidth=900",
-    "https://i.pinimg.com/originals/71/38/54/71385477859ae0e77b6bb31028f16822.jpg",
     "https://pbs.twimg.com/profile_images/1186791037928378369/Ni5CAqw5.jpg",
     "https://avatars.githubusercontent.com/u/88138569?s=400&u=c323f2140120fcca7448b058207d818eeb619e57&v=4"
 ];
@@ -27,16 +26,22 @@ for(let i = 0; i < headerList.length; i++) {
 // Get Extension Form Data
 const wrapper = document.querySelector(".wrapper"),
     form = wrapper.querySelectorAll(".registration-form"),
-    submitInput = form[0].querySelector("input[type='submit'")
+    submitInput = form[0].querySelector("input[type='submit'"),
+    count = wrapper.querySelector("#count");
 
 function getFormData(e) {
     e.preventDefault();
 
     var formData = new FormData(form[0]);
-
     var rating = formData.get('rate');
     
-    alert(rating);
+    if(rating=="Yes") {
+        count.textContent++;
+    } else if(rating=="No") {
+        count.textContent--;
+    } else {
+        //
+    }
 }
 
 document.addEventListener('DOMContentLoaded', function(){
